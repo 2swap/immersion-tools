0333173bb7acdab1fe94954bd172938dc638083b
2swap <mchontz10@gmail.com>
2023-02-12 16:29:07 -0800

    made subs2srs with bash

---
+++ subs2srs.sh
@@ -0,0 +1,39 @@
+echo
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: subs2srs your/video/file.mkv"
+    exit
+fi
+
+ffprobe "$1" 2>&1 | grep Stream | grep Audio
+ffprobe "$1" 2>&1 | grep Stream | grep Subtitle
+
+echo
+
+if [ "$#" -ne 4 ]; then
+    echo "Usage: subs2srs your/video/file.mkv audio_stream_number subtitle_stream_number media_prefix"
+    echo
+    exit
+fi
+
+#Stream #0:1(jpn): Audio: aac (LC), 48000 Hz, stereo, fltp (default)
+aud_ext=$(ffprobe "$1" 2>&1 | grep Stream | grep 0:$2 | sed -rn 's/.*Audio: (...).*/\1/p')
+subs=anki/subs.$(ffprobe "$1" 2>&1 | grep Stream | grep 0:$3 | sed -rn 's/.*Subtitle: (...).*/\1/p')
+
+mkdir -p anki/audio
+rm $subs
+
+ffmpeg -loglevel error -i "$1" -map 0:$3 $subs
+
+i=0
+while IFS= read -r line; do
+    starttime=$(echo $line | sed -rn 's/[^,]*,([^,]*),([^,]*),[^,]*,[^,]*,[^,]*,[^,]*,[^,]*,[^,]*,[^,]*,(.*)/\1/p')
+    [ -z "$starttime" ] && continue
+    endtime=$(echo $line | sed -rn 's/[^,]*,([^,]*),([^,]*),[^,]*,[^,]*,[^,]*,[^,]*,[^,]*,[^,]*,[^,]*,(.*)/\2/p')
+    text=$(echo $line | sed -rn 's/[^,]*,([^,]*),([^,]*),[^,]*,[^,]*,[^,]*,[^,]*,[^,]*,[^,]*,[^,]*,(.*)/\3/p')
+    audiofilename="$4$i.$aud_ext"
+    echo "[sound:$audiofilename]\t$text" >> anki/import_to_anki.tsv
+    ffmpeg -i "$1" -ss "$starttime"0 -to "$endtime"0 -q:a 0 -map a anki/audio/$audiofilename && ((i++))
+done < $subs
+
+

092d4e46b835b1aaa210fc3bbf355bd4329d6a7b
2swap <mchontz10@gmail.com>
2023-09-04 11:12:33 -0700

    conf

--- yt-sync/yt-sync.sh
+++ yt-sync/yt-sync.sh
@@ -1,9 +1,13 @@
-while read p; do
-	[[ $p =~ ^#.* ]] && continue
-	simulation="-s --get-filename" # insert into ytdlp to simulate filename outputs
-	disk_location=$(echo "$p" | cut -f2 | sed 's/ /_/g')
-	channel=$(echo "$p" | cut -f1)
-	extra_args=$(echo "$p" | cut -f3-)
-	echo "Syncing channel $channel at disk location $disk_location, with extra args $extra_args"
-	yt-dlp --all-subs $extra_args --download-archive archive.txt -o $disk_location $channel
+#!/bin/bash
+
+# Read the config file line by line, splitting fields using tab as the delimiter
+while IFS=$'\t' read -r url disk_location extra_args; do
+    # Skip lines that start with a '#' (comments)
+    [[ $url =~ ^#.* ]] && continue
+        
+    # Echo information about the current synchronization
+    echo "Syncing URL $url at disk location $disk_location, with extra args $extra_args"
+    
+    # Call yt-dlp with provided arguments and options
+    yt-dlp --all-subs $extra_args --download-archive archive.txt --cookies-from-browser chrome -o "$disk_location" "$url"
 done <config.tsv
